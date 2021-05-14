fe1bba6c3dd9add7427c3d1cf2e539d1bfe99a8b
Анна Чурина <anna.churina@nw.rt.ru>
2021-05-14 23:13:44 +0300

    pom

--- src/main/java/ru/plantarum/core/web/CounterAgentController.java
+++ src/main/java/ru/plantarum/core/web/CounterAgentController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.plantarum.core.entity.CounterAgent;
 import ru.plantarum.core.entity.CounterAgentNote;
 import ru.plantarum.core.entity.CounterAgentType;
--- src/main/resources/static/js/paste.js
+++ src/main/resources/static/js/paste.js
@@ -3,14 +3,17 @@ $("#note").bind("paste", function(e){
  e.preventDefault();
     // access the clipboard using the api
     var pastedData = e.originalEvent.clipboardData.getData('text');
-    var href = $(this).attr('href');
+
      var maxLength = $(this).attr('maxlength');
-               console.log(maxLength);
                if (pastedData.length > maxLength) {
                    $(this).val(pastedData.substring(0, maxLength));
                }
-               $.post(href, { note: $(this).val() } );
+//               $.post(href, { note: $(this).val() } );
 
 } );
-
+ $("#add-note").click(function(){
+ var textarea =$("#note");
+ var href =textarea.attr('href');
+        $.post(href, { note: textarea.val() } );
+    });
 });
