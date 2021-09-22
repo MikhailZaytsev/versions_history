f644bd6f8a58cb164ea8dc6b28ca929515a08b06
Михаил <mikhailzaytsev02.1996@gmail.com>
2021-09-22 13:41:44 +0300

    22/09/2021/1.1 Дядя Коля смотрел версию, многое не работает

--- src/main/java/ru/plantarum/core/notImportant/SomethingMine.java
+++ src/main/java/ru/plantarum/core/notImportant/SomethingMine.java
@@ -2,6 +2,7 @@ package ru.plantarum.core.notImportant;
 
 public class SomethingMine {
     int del = 54;
+    String in = "branches with date";
 
     public void printMsg() {
         String msg = "trtrt";
