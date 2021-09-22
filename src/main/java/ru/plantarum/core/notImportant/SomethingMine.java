7b4d71ae215913ba6e30d42d43f9295181236ddd
Михаил <mikhailzaytsev02.1996@gmail.com>
2021-09-22 13:36:56 +0300

    22/09/2021/1 Дядя Коля смотрел версию, многое не работает

--- src/main/java/ru/plantarum/core/notImportant/SomethingMine.java
+++ src/main/java/ru/plantarum/core/notImportant/SomethingMine.java
@@ -1,10 +1,13 @@
 package ru.plantarum.core.notImportant;
 
 public class SomethingMine {
-    String msg = "la";
+    int del = 54;
 
     public void printMsg() {
-        msg = "trtrt";
-        System.out.println(msg);
+        String msg = "trtrt";
+        while (del <= 60) {
+            System.out.println(msg);
+            del += 5;
+        }
     }
 }
